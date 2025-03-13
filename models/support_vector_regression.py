c2c4b5e6f7ba745bcc929c82746685372b1b7a1c
orlandodileo <136668652+orlandodileo@users.noreply.github.com>
2025-03-12 19:55:16 -0500

    Coded dummy awards and implemented to models.

--- models/support_vector_regression.py
+++ models/support_vector_regression.py
@@ -27,7 +27,8 @@ df["previous_rating"] = df["previous_rating"].fillna(0)
 NUM_VARS = ["age","agesq","games","minutes_played_per_game",
             "field_goal_percentage","three_point_field_goal_attempts_per_game",
             "three_point_field_goal_percentage","total_rebounds_per_game","assists_per_game","steals_per_game",
-            "blocks_per_game","turnovers_per_game","points_per_game","previous_rating", "has_previous_rating"]
+            "blocks_per_game","turnovers_per_game","points_per_game","previous_rating", "has_previous_rating", "AS",
+            "MVP","DPOY"]
 
 CAT_VARS = ["position"]
 
@@ -36,12 +37,10 @@ X = df[[
     "three_point_field_goal_attempts_per_game", "three_point_field_goal_percentage",
     "total_rebounds_per_game", "assists_per_game", "steals_per_game",
     "blocks_per_game", "turnovers_per_game", "points_per_game",
-    "previous_rating", "has_previous_rating", "position"
+    "previous_rating", "has_previous_rating", "position", "AS","MVP","DPOY"
 ]]
 y = df["rating"]
 
-X = X[y.notna()]
-y = y[y.notna()]
 X = X.fillna(0)
 
 X_train, X_test, y_train, y_test= train_test_split(X, y, random_state=42)
